0cdcd4fb60bc0c89b3771050374839c507464393
vinay-3708 <85564385+vinay-3708@users.noreply.github.com>
2023-03-27 00:05:14 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello First World');
+  res.send('Hello World');
 });
 
 /* istanbul ignore next */
